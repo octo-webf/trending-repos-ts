87e220f845317ce94d8b1828ec4ec5fcc75f1966
abkaoudjt <abkaoudjt@octo.com>
2019-07-26 11:12:50 +0200

    plug trends service into component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import { TrendsService } from '../services/TrendsService'
 
-export interface AppProps {}
+export interface AppProps {
+  trendsService: TrendsService
+}
 
-export const App: React.FC<AppProps> = () => <h1>Hello world</h1>;
+export const App: React.FC<AppProps> = () => {
+  useEffect(() => {
+
+  }, [])
+
+  return <h1>hello world</h1>
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
-import "./index.css";
+import './index.css'
 
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { App } from "./components/App";
+import { App } from './components/App'
+import { HttpClient } from './services/HttpClient'
+import { TrendsService } from './services/TrendsService'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const httpClient: HttpClient = {
+  get<T>(url: string): Promise<T> {
+    return fetch(url).then(response => response.json() as Promise<T>)
+  }
+
+}
+
+ReactDOM.render(<App trendsService={new TrendsService(httpClient)}/>, document.getElementById('root'))
