6d9aaaf73bd4d2f8b0577e5b13e18812484ddd96
Antoine Chalifour <achalifour@octo.com>
2019-08-22 15:22:22 +0200

    live-coding template

--- src/services/TrendsService.test.ts
+++ src/services/TrendsService.test.ts
@@ -1,67 +1,55 @@
-import { TrendsService } from './TrendsService'
-import { HttpClient } from "./HttpClient";
-import { Trend } from './models'
-
 describe("TrendsService", () => {
   describe("fetchAll", () => {
     it("should return the trends", async () => {
       // Given
-      const url = "https://github-trending-api.now.sh/";
-      const expectedResult: Trend[] = [
-        {
-          author: {
-            name: "John Doe",
-            avatar: "http://my-avatar.com/1"
-          },
-          repository: {
-            name: "React",
-            url: "http://repo.com/1",
-            description: "React description",
-            stars: 12
-          }
-        },
-        {
-          author: {
-            name: "Sara Walker",
-            avatar: "http://my-avatar.com/2"
-          },
-          repository: {
-            name: "Typescript",
-            url: "http://repo.com/2",
-            description: "Typescript description",
-            stars: 120
-          }
-        }
-      ]
-      const httpClient: HttpClient = {
-        get: jest.fn().mockResolvedValue([
-          {
-            author: "John Doe",
-            avatar: "http://my-avatar.com/1",
-            name: "React",
-            url: "http://repo.com/1",
-            description: "React description",
-            stars: 12
-          },
-          {
-            author: "Sara Walker",
-            avatar: "http://my-avatar.com/2",
-            name: "Typescript",
-            url: "http://repo.com/2",
-            description: "Typescript description",
-            stars: 120
-          }
-        ])
-      };
-      const trendsService = new TrendsService(httpClient);
-
+      // const expectedResult: Trend[] = [
+      //   {
+      //     author: {
+      //       name: "John Doe",
+      //       avatar: "http://my-avatar.com/1"
+      //     },
+      //     repository: {
+      //       name: "React",
+      //       url: "http://repo.com/1",
+      //       description: "React description",
+      //       stars: 12
+      //     }
+      //   },
+      //   {
+      //     author: {
+      //       name: "Sara Walker",
+      //       avatar: "http://my-avatar.com/2"
+      //     },
+      //     repository: {
+      //       name: "Typescript",
+      //       url: "http://repo.com/2",
+      //       description: "Typescript description",
+      //       stars: 120
+      //     }
+      //   }
+      // ]
+      // const mockResolvedValue = [
+      //   {
+      //     author: "John Doe",
+      //     avatar: "http://my-avatar.com/1",
+      //     name: "React",
+      //     url: "http://repo.com/1",
+      //     description: "React description",
+      //     stars: 12
+      //   },
+      //   {
+      //     author: "Sara Walker",
+      //     avatar: "http://my-avatar.com/2",
+      //     name: "Typescript",
+      //     url: "http://repo.com/2",
+      //     description: "Typescript description",
+      //     stars: 120
+      //   }
+      // ]
       // When
-      const response = await trendsService.fetchAll();
-
+      // TODO: appeler le service
       // Then
-      expect(httpClient.get).toHaveBeenCalledTimes(1);
-      expect(httpClient.get).toHaveBeenNthCalledWith(1, url);
-      expect(response).toEqual(expectedResult);
+      // TODO: expectations
     });
   });
 });
--- src/services/TrendsService.ts
+++ src/services/TrendsService.ts
@@ -1,36 +1,8 @@
-import { HttpClient } from './HttpClient'
-import { Trend } from './models'
-
 export interface ApiTrendResponse {
-  author: string;
-  name: string;
-  avatar: string;
-  url: string;
-  description: string;
-  stars: number;
+  // TODO: utiliser la réponse pour générer l'interface
 }
 
 export class TrendsService {
-  public constructor(private httpClient: HttpClient) {}
-
-  public fetchAll() {
-    return this.httpClient
-      .get<ApiTrendResponse[]>("https://github-trending-api.now.sh/")
-      .then(trends => trends.map(this.mapApiTrendToTrend));
-  }
-
-  private mapApiTrendToTrend(trend: ApiTrendResponse): Trend {
-    return {
-      author: {
-        avatar: trend.avatar,
-        name: trend.author
-      },
-      repository: {
-        description: trend.description,
-        name: trend.name,
-        stars: trend.stars,
-        url: trend.url
-      }
-    };
-  }
+  // TODO: constructeur
+  // TODO: fetchAll() -> https://github-trending-api.now.sh/
 }
